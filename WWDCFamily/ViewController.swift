87dad6d726be405625840d9f53bd482ac9acfe3b
Elvis Nuñez <elvisnunez@me.com>
2017-02-17 13:58:05 +0100

    First commit

--- WWDCFamily/ViewController.swift
+++ WWDCFamily/ViewController.swift
@@ -1,25 +1,11 @@
-//
-//  ViewController.swift
-//  WWDCFamily
-//
-//  Created by Elvis Nuñez on 17/02/2017.
-//  Copyright © 2017 WWDC Family. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        view.backgroundColor = .red
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
