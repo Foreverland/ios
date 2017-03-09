a5e867f821bba86c7865ca0bed433d455434c288
Elvis Nuñez <elvisnunez@me.com>
2017-03-10 00:42:52 +0100

    Add Firebase

---
+++ Podfile
@@ -0,0 +1,6 @@
+target 'WWDCFamily' do
+  use_frameworks!
+
+  pod 'Firebase/Core', '~> 3.0'
+  pod 'FirebaseUI', '~> 3.0'
+end
--- WWDCFamily/Source/Main/AppDelegate.swift
+++ WWDCFamily/Source/Main/AppDelegate.swift
@@ -1,4 +1,5 @@
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -6,6 +7,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        FIRApp.configure()
 
         window = UIWindow(frame: UIScreen.main.bounds)
 
